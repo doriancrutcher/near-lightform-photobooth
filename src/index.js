31ee77c5f24ef6e007034b991e855b40082ea61a
Dorian Kinoo Crutcher <doriankinoocrutcher@gmail.com>
2021-05-31 09:07:11 -0700

    updated config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "regenerator-runtime/runtime";
 import { initContract, login, logout } from "./utils";
 
 import getConfig from "./config";
-const { networkId } = getConfig(process.env.NODE_ENV || "production");
+const { networkId } = getConfig(process.env.NODE_ENV || "mainnet");
 
 // global variable used throughout
 let currentGreeting;
